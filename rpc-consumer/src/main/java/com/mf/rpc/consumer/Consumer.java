84fdd0ef90c63fa848e2c0575db97ad5455dd5b7
pingzhu <ping.zhu@microfocus.com>
2022-06-21 10:11:45 +0800

    add zk listener

--- rpc-consumer/src/main/java/com/mf/rpc/consumer/Consumer.java
+++ rpc-consumer/src/main/java/com/mf/rpc/consumer/Consumer.java
@@ -5,10 +5,11 @@ import com.mf.rpc.api.UserService;
 import com.mf.rpc.core.client.RpcClient;
 
 public class Consumer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         RpcClient rpcClient = new RpcClient("localhost:2181");
         UserService userService = rpcClient.createFromRegistry(UserService.class);
         User user = userService.findById(1);
         System.out.println("user::" + user.getName());
+        Thread.sleep(1000000);
     }
 }
--- rpc-core/src/main/java/com/mf/rpc/core/client/RpcClient.java
+++ rpc-core/src/main/java/com/mf/rpc/core/client/RpcClient.java
@@ -11,6 +11,7 @@ import com.mf.rpc.core.zk.ZkSerializer;
 import lombok.Getter;
 import lombok.Setter;
 import okhttp3.*;
+import org.I0Itec.zkclient.IZkDataListener;
 import org.I0Itec.zkclient.ZkClient;
 import org.I0Itec.zkclient.serialize.SerializableSerializer;
 
@@ -20,6 +21,9 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 public class RpcClient {
 
@@ -31,7 +35,7 @@ public class RpcClient {
 
     @Getter
     @Setter
-    private List<String> urls = new ArrayList<>();
+    private Map<String, String> servers = new ConcurrentHashMap();
     @Getter
     @Setter
     private String zkUrl;
@@ -49,15 +53,36 @@ public class RpcClient {
 
         List<String> children =zkClient.getChildren(ZK_ROOT);
         children.forEach(c -> {
-            String url = zkClient.readData(ZK_ROOT + "/" + c);
-            urls.add(url);
+            String childrenPath = ZK_ROOT + "/" + c;
+            String url = zkClient.readData(childrenPath);
+            servers.put(childrenPath, url);
+
+            IZkDataListener dataListener = new IZkDataListener() {
+                @Override
+                public void handleDataChange(String s, Object o) throws Exception {
+                    System.out.println("zk change data" + s);
+                    servers.put(s, (String) o);
+                }
+
+                @Override
+                public void handleDataDeleted(String s) throws Exception {
+                    System.out.println("zk delete data");
+                    servers.remove(s);
+                }
+            };
+            zkClient.subscribeDataChanges(childrenPath, dataListener);
         });
     }
 
     public <T> T createFromRegistry(final Class<T> clazz,  Filter ...filters) {
 
         Router router = new RouterImpl();
-        String url = router.route(this.getUrls());
+        List<String> urls = this.getServers()
+                .keySet()
+                .stream()
+                .map(key -> this.getServers().get(key))
+                .collect(Collectors.toList());
+        String url = router.route(urls);
         return create(clazz, url, filters);
     }
 
